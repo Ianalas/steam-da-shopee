23d8d0a83316c24734498254153fdf8d5f5547cb
pedrooaj <pedroantonio5735@gmail.com>
2025-09-30 07:29:19 -0300

    Feat: configurando o swagger

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ async function bootstrap() {
   }));
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Steam da Shopee API')
+    .setDescription('Steam da Shopee API descrição')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('steam')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
